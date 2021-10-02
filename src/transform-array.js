3d4de7d01baea06a3d5fc170475f03a3c83c3cd1
Diana Khlomko <kim.keiraff@gmail.com>
2021-10-02 17:58:19 +0300

    feat: transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+    if (!Array.isArray(arr)) throw Error('\'arr\' parameter must be an instance of the Array!');
+    let modifiedArray = new Array();
+    for (var i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                if (arr[i - 2] !== '--discard-next') modifiedArray.pop();
+                break;
+            case '--double-next':
+                if (arr[i + 1]) modifiedArray.push(arr[i + 1]);
+                break;
+            case '--double-prev':
+                if (arr[i - 1] && arr[i - 2] !== '--discard-next') modifiedArray.push(arr[i - 1]);
+                break;
+            default:
+                modifiedArray.push(arr[i]);
+                break;
+        }
+    }
+    return modifiedArray;
 }
